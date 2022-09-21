4e19f8ccb3c71454a83ea0d040a09bbe866efbb0
duffaldri <mdaffaaldriantama@gmail.com>
2022-09-21 15:09:42 +0700

    Tried to move the object using WASD, but failed :(

--- belajar-di-kelas/main.js
+++ belajar-di-kelas/main.js
@@ -3,10 +3,10 @@ function main() {
     var gl = kanvas.getContext("webgl");
 
     var vertices = [
-        0.5, 0.5, 0.0, 1.0, 1.0,  // A: CYAN
-        0.0, 0.0, 1.0, 0.0, 1.0,// B: MAGENTA
-        -0.5, 0.5, 1.0, 1.0, 0.0,// C: KUNING
-        0.0, 1.0, 1.0, 1.0, 1.0 // PUTIH
+        0.5, 0, 0.0, 1.0, 1.0,  // A: CYAN
+        0.0, -0.5, 1.0, 0.0, 1.0,// B: MAGENTA
+        -0.5, 0, 1.0, 1.0, 0.0,// C: KUNING
+        0.0, 0.5, 1.0, 1.0, 1.0 // PUTIH
     ];
 
     var buffer = gl.createBuffer();
@@ -17,11 +17,13 @@ function main() {
     attribute vec2 aPosition; // Untuk posisi titiknya
     attribute vec3 aColor;
     uniform float uTheta;
+    uniform float uHorizontal;
+    uniform float uVertical;
     varying vec3 vColor;
 
     void main() {
-        float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
-        float y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x;
+        float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y + uHorizontal;
+        float y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x + uVertical;
         gl_Position = vec4(x, y, 0.0, 1.0); // Untuk mendapatkan x & y, bisa menggunakan x, y atau aPosition.xy atau aPosition
         vColor = aColor;
     } 
@@ -54,9 +56,15 @@ function main() {
 
     // Variabel lokal
     var theta = 0.0;
+    var horizontal = 0.0;
+    var vertical = 0.0;
+    var freeze = false;
     
     // Variabel pointer ke GLSL
     var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
+    var uHorizontal = gl.getUniformLocation(shaderProgram, "uHorizontal");
+    var uVertical = gl.getUniformLocation(shaderProgram, "uVertical");
+
 
     //Kita mengajari GPU bagaimana caranya mengoleksi
     // nilai posisi dari ARRAY_BUFFER
@@ -76,6 +84,40 @@ function main() {
         2 * Float32Array.BYTES_PER_ELEMENT); // 2 karena ...
     gl.enableVertexAttribArray(aColor);
 
+    function onMouseClick(event) {
+        freeze = !freeze;
+    }
+
+    document.addEventListener("click", onMouseClick, false);
+
+
+    function onKeyDown(event) {
+        if(event.keyCode == 87) { // W
+
+        } 
+        else if(event.keyCode == 65) { // A
+            uHorizontal = -0.001;
+        } else if(event.keyCode == 83) { // S
+
+        } else if(event.keyCode == 68) { // D
+
+        }
+    }
+    document.addEventListener("keydown", onKeyDown, false);
+
+    function onKeyDown(event) {
+        if(event.keyCode == 87) { // W
+            uVertical = 0;
+        } else if(event.keyCode == 65) { // A
+            uHorizontal = 0;
+        } else if(event.keyCode == 83) { // S
+            uVertical = 0;
+        } else if(event.keyCode == 68) { // D
+            uHorizontal = 0;
+        }
+    }
+    document.addEventListener("keydown", onKeyDown, false);
+
     function render() {
         // // Cara 2, menggunakan timeout
         // setTimeout(function() {
@@ -93,17 +135,20 @@ function main() {
         gl.clearColor(1, 0.6, 0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
         
-        theta += 0.0005; // + itu counter-clockwise, - clockwise
-        gl.uniform1f(uTheta, theta);
-
+        if(!freeze) {
+            theta += 0.01; // + itu counter-clockwise, - clockwise
+            gl.uniform1f(uTheta, theta);
+        }
+        
+        gl.uniform1f(uHorizontal, horizontal);
         gl.drawArrays(gl.TRIANGLE_FAN ,     0,      4);
         //          PRIMITIVE,                 banyaknya titik
 
-        // requestAnimationFrame(render);
+        requestAnimationFrame(render);
     }
-    // requestAnimationFrame(render); // Dengan menggunakan requestAnimationFrame, browser akan berhenti selama 1/60 detik setelah menjalankan fungsi
+    requestAnimationFrame(render); // Dengan menggunakan requestAnimationFrame, browser akan berhenti selama 1/60 detik setelah menjalankan fungsi
 
-    setInterval(render, 1000/120) // Cara 1, panggil fungsi tiap 1000/60 ms atau 1/60 detik
+    // setInterval(render, 1000/120) // Cara 1, panggil fungsi tiap 1000/60 ms atau 1/60 detik
 
 
 
